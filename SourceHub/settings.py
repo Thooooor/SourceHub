043ca55ca0b5074cec4d64d85952cc6fc7cfa274
Thooooor <46197373+Thooooor@users.noreply.github.com>
2021-04-09 23:09:52 +0800

    Change Database to MySQL

--- SourceHub/settings.py
+++ SourceHub/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'SourceHub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sourcehub',
+        'USER': 'root',
+        'PASSWORD': '991229',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
