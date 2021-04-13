119756bbf678d0365e654b2c69b5e36bfa68a1fa
Thooooor <46197373+Thooooor@users.noreply.github.com>
2021-04-13 23:18:26 +0800

    Beatify index

--- home/views.py
+++ home/views.py
@@ -43,6 +43,10 @@ def get_hot_sources():
     if len(hot_sources) > 7:
         hot_sources = hot_sources[:7]
 
+    for source in hot_sources:
+        if source.download_counts > 0:
+            source.ratio = source.download_counts / max_downloads * 86
+
     return hot_sources, max_downloads
 
 
@@ -50,10 +54,14 @@ def get_hot_courses():
     hot_courses = list(Course.objects.all())
     if len(hot_courses) == 0:
         return hot_courses, 0
-    hot_courses.sort(key=lambda course: course.students.count(), reverse=True)
+    hot_courses.sort(key=lambda hot_course: hot_course.students.count(), reverse=True)
     max_students = hot_courses[0].students.count()
     if len(hot_courses) > 7:
         hot_courses = hot_courses[:7]
+
+    for course in hot_courses:
+        if course.students.count() > 0:
+            course.ratio = course.students.count() / max_students * 70
 
     return hot_courses, max_students
 
@@ -62,9 +70,13 @@ def get_max_courses():
     max_courses = list(Course.objects.all())
     if len(max_courses) == 0:
         return max_courses, 0
-    max_courses.sort(key=lambda course: course.sources.count(), reverse=True)
+    max_courses.sort(key=lambda max_course: max_course.sources.count(), reverse=True)
     max_course_sources = max_courses[0].students.count()
     if len(max_courses) > 7:
         max_courses = max_courses[:7]
+
+    for course in max_courses:
+        if course.sources.count() > 0:
+            course.ratio = course.sources.count() / max_course_sources * 80
 
     return max_courses, max_course_sources
